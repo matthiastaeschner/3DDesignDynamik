d4f47223020ceef73d964867ceeaad0b52db1ddf
matthiastaeschner <matthias.taeschner@posteo.de>
2017-09-15 15:24:14 +0200

    AnimatorController für Ritter

--- 3DDesignDynamik_DracheRitter/Assets/Scripts/Knight/ComputerControlledKnight.cs
+++ 3DDesignDynamik_DracheRitter/Assets/Scripts/Knight/ComputerControlledKnight.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class ComputerControlledKnight : MonoBehaviour {
 
+	private Animator anim;
+
 	// Use this for initialization
 	void Start () {
 		if (Debug.isDebugBuild) {
 			Debug.Log ("ComputerControlledKnight-Script attached to " + gameObject.name);
 		}
+
+		anim = gameObject.GetComponent<Animator> ();
+
+		anim.Play ("Knight_Stand_Sword");
 	}
 	
 	// Update is called once per frame
--- 3DDesignDynamik_DracheRitter/Assets/Scripts/Knight/PlayerControlledKnight.cs
+++ 3DDesignDynamik_DracheRitter/Assets/Scripts/Knight/PlayerControlledKnight.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class PlayerControlledKnight : MonoBehaviour {
 
+	private Animator anim;
+
 	// Use this for initialization
 	void Start () {
 		if (Debug.isDebugBuild) {
 			Debug.Log ("PlayerControlledKnight-Script attached to " + gameObject.name);
 		}
+		anim = gameObject.GetComponent<Animator> ();
+
+		anim.Play ("Knight_Stand_Sword");
 	}
 	
 	// Update is called once per frame
